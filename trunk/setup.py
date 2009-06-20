f0a3b0de713c54066bd5fb1a896aa80bc0ce321a
LTsmooth42 <LTsmooth42@26d1a0c0-5d46-11de-bc7c-c9df4fa8b3c6>
2009-06-20 03:07:27 +0000

    Version bump to 0.2.2; updated about dialog

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,13 +2,13 @@
 from distutils.core import setup
 
 setup(name="griver",
-		version="0.2.1",
+		version="0.2.2",
 		scripts = ['griver'],
 		data_files=[('share/griver/', ['griver.glade']),
                 ('share/pixmaps/', ['griver.svg']),
       			    ('share/applications/', ['griver.desktop'])],
 		author = "Lukas Sabota",
-		author_email = "punkrockguy318@comcast.net",
-		url = "http://dietschnitzel.com/griver"
+		author_email = "ltsmooth42 _at_ gmail _dot_ com",
+		url = "http://code.google.com/p/griver/"
 		)
 
